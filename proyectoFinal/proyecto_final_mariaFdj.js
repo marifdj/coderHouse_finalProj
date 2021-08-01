f6edb204458fe988db6596c3277d878f472c219f
Maria Florencia De Juan <florenciadejuan@gmail.com>
2021-08-01 00:25:05 -0300

    new changes, carrito de compras

--- proyectoFinal/proyecto_final_mariaFdj.js
+++ proyectoFinal/proyecto_final_mariaFdj.js
@@ -1,87 +1,200 @@
-/*
->>Objetivos generales:
-Codificar la funcionalidad inicial del simulador. 
-Identificar el flujo de trabajo del script en términos de captura de entradas ingresadas por el usuario, procesamiento esencial del simulador y notificación de resultados en forma de salida.
->>Objetivos específicos:
-Capturar entradas mediante prompt().
-Declarar variables y objetos necesarios para simular el proceso seleccionado.
-Crear funciones y/o métodos para realizar operaciones (suma, resta, concatenación, división, porcentaje, etc).
-Efectuar una salida, que es el resultado de los datos procesados, la cual puede hacerse por alert() o console.log().
+//PROYECTO FINAL CERVECERIA LOTHBROK
+    const baseDeDatos = [
+        { id: 1, nombre: 'Honey', precio: 250, imagen: 'rubia.jpg'},
+        { id: 2, nombre: 'Negra', precio: 370, imagen: 'negra.jpg'},
+        { id: 3, nombre: 'Ipa', precio: 380, imagen: 'ipa.jpg'},
+        { id: 4, nombre: 'Roja', precio: 220, imagen:'ipa.jpg'}
+];
 
-*/
+    let carrito = [];
+    let total = 0;
+    const DOMitems = document.querySelector('#items');
+    const DOMcarrito = document.querySelector('#carrito');
+    const DOMtotal = document.querySelector('#total');
+    const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+    const miLocalStorage = window.localStorage;
+    const principal= ("#btnPrincipal");
+    const ayuda= ("#btnAyuda");
+    const reservas= ("#btnReservas");
+    const contacto= ("#btnContacto");
 
-//Array de bebidas
-class Bebida {
-    constructor(nombre, precio) {
-        this.nombre  = nombre.toUpperCase();
-        this.precio  = parseFloat(precio);
-    }
-        sumaIva() {
-        this.precio = this.precio * 1.21;
-    }
-}
 
-function saludar(){
-    alert('Los sabores de cerveza son: IPA, Honey y Negra')    
-    let valor = prompt('Ingresar el producto que quiere seleccionar');
-    if(valor=== 'IPA' && valor !=null){
-        alert('Su cerveza elegida es IPA!');
+    // Funciones
+//agrega los productos al html
+ 
+    function renderizarProductos() {
+        baseDeDatos.forEach((info) => {
+            // Estructura
+            const miNodo = document.createElement('div');
+            miNodo.classList.add('card', 'col-sm-4');
+            // Body
+            const miNodoCardBody = document.createElement('div');
+            miNodoCardBody.classList.add('card-body');
+            // Titulo
+            const miNodoTitle = document.createElement('h5');
+            miNodoTitle.classList.add('card-title');
+            miNodoTitle.textContent = info.nombre;
+            // Imagen
+            const miNodoImagen = document.createElement('img');
+            miNodoImagen.classList.add('img-fluid');
+            miNodoImagen.setAttribute('src', info.imagen);
+            // Precio
+            const miNodoPrecio = document.createElement('p');
+            miNodoPrecio.classList.add('card-text');
+            miNodoPrecio.textContent = '$' +info.precio ;
+            // Boton 
+            const miNodoBoton = document.createElement('button');
+            miNodoBoton.classList.add('btn', 'btn-primary');
+            miNodoBoton.textContent = '+';
+            miNodoBoton.setAttribute('marcador', info.id);
+            miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+            // Insertamos
+            miNodoCardBody.appendChild(miNodoImagen);
+            miNodoCardBody.appendChild(miNodoTitle);
+            miNodoCardBody.appendChild(miNodoPrecio);
+            miNodoCardBody.appendChild(miNodoBoton);
+            miNodo.appendChild(miNodoCardBody);
+            DOMitems.appendChild(miNodo);
+        });
     }
-    if (valor==='Honey' && valor !=null){
-        alert('Su cerveza es Honey!')
+
+//Evento para añadir un producto al carrito de la compra
+
+    function anyadirProductoAlCarrito(evento) {
+        // Anyadimos el Nodo a nuestro carrito
+        carrito.push(evento.target.getAttribute('marcador'))
+        // Calculo el total
+        calcularTotal();
+        // Actualizamos el carrito 
+        renderizarCarrito();
+        // Actualizamos el LocalStorage
+        guardarCarritoEnLocalStorage();
     }
-    if (valor==='Negra' && valor !=null){
-        alert('Su cerveza es Negra!')
+
+    function renderizarCarrito() {
+        // Vaciamos todo el html
+        DOMcarrito.textContent = '';
+        // Quitamos los duplicados
+        const carritoSinDuplicados = [...new Set(carrito)];
+        // Generamos los Nodos a partir de carrito
+        carritoSinDuplicados.forEach((item) => {
+            // Obtenemos el item que necesitamos de la variable base de datos
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                // ¿Coincide las id? Solo puede existir un caso
+                return itemBaseDatos.id === parseInt(item);
+            });
+            // Cuenta el número de veces que se repite el producto
+            const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+                // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+                return itemId === item ? total += 1 : total;
+            }, 0);
+            // Creamos el nodo del item del carrito
+            const miNodo = document.createElement('li');
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}`;
+            // Boton de borrar
+            const miBoton = document.createElement('button');
+            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+            miBoton.textContent = 'X';
+            miBoton.style.marginLeft = '1rem';
+            miBoton.dataset.item = item;
+            miBoton.addEventListener('click', borrarItemCarrito);
+            // Mezclamos nodos
+            miNodo.appendChild(miBoton);
+            DOMcarrito.appendChild(miNodo);
+        });
+    }
+
+    function borrarItemCarrito(evento) {
+        // Obtenemos el producto ID que hay en el boton pulsado
+        const id = evento.target.dataset.item;
+        // Borramos todos los productos
+        carrito = carrito.filter((carritoId) => {
+            return carritoId !== id;
+        });
+        // volvemos a renderizar
+        renderizarCarrito();
+        // Calculamos de nuevo el precio
+        calcularTotal();
+        // Actualizamos el LocalStorage
+        guardarCarritoEnLocalStorage();
+
+    }
+
+    function calcularTotal() {
+        // Limpiamos precio anterior
+        total = 0;
+        // Recorremos el array del carrito
+        carrito.forEach((item) => {
+            // De cada elemento obtenemos su precio
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                return itemBaseDatos.id === parseInt(item);
+            });
+            total = total + miItem[0].precio;
+        });
+        // Renderizamos el precio en el HTML
+        DOMtotal.textContent = total.toFixed(2);
+    }
+
+    function vaciarCarrito() {
+        // Limpiamos los productos guardados
+        carrito = [];
+        // Renderizamos los cambios
+        renderizarCarrito();
+        calcularTotal();
+        // Borra LocalStorage
+        localStorage.clear();
+
+    }
+
+    function guardarCarritoEnLocalStorage () {
+        miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+    }
+
+    function cargarCarritoDeLocalStorage () {
+        // ¿Existe un carrito previo guardado en LocalStorage?
+        if (miLocalStorage.getItem('carrito') !== null) {
+            // Carga la información
+            carrito = JSON.parse(miLocalStorage.getItem('carrito'));
         }
     }
- saludar();   
-//Declaramos un array de productos para almacenar objetos
-const productos = [];
 
-productos.push(new Bebida("Honey", 850));
-productos.push(new Bebida("IPA", 340));
-productos.push(new Bebida("Negra", 350));
+    // Eventos
+    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
 
-
-for (const producto of productos) {
-    producto.sumaIva();
-    console.log(producto);
-}
+    // Inicio
+    cargarCarritoDeLocalStorage();
+    renderizarProductos();
+    calcularTotal();
+    renderizarCarrito();
 
 
-var ordenarMenorPrecio= [];
-ordenarMenorPrecio= productos.map(elemento => elemento );
-var ordenarMenorPrecio = productos;
-ordenarMenorPrecio.sort(function(a, b){
-return a.precio - b.precio;
-})
-
-console.log('Ordenados por menor valor');
-console.log(ordenarMenorPrecio);
-
+ 
 const tipoDeProducto= [ {id:1, nombre: "lata 750ml", precio: 250},
-                        {id:2, nombre: "barril 50lts", precio: 2500},
-                        {id:3, nombre: "barril 100lts", precio: 4500},
+{id:2, nombre: "barril 50lts", precio: 2500},
+{id:3, nombre: "barril 100lts", precio: 4500},
 
 ];
 for (const producto of tipoDeProducto) {
-    let contenedor = document.createElement("div");
-    //Definimos el innerHTML del elemento con una plantilla de texto
-    contenedor.innerHTML = `<h3> ${producto.id}</h3>
-                            <p>  Producto: ${producto.nombre}</p>
-                            <b> $ ${producto.precio}</b>`;
-    document.body.appendChild(contenedor);
+let contenedor = document.createElement("div");
+//Definimos el innerHTML del elemento con una plantilla de texto
+contenedor.innerHTML = `<h3> ${producto.id}</h3>
+    <p>  Producto: ${producto.nombre}</p>
+    <b> $ ${producto.precio}</b>`;
+document.body.appendChild(contenedor);
 }
 
 function eventAction(element){
-    let boton         = document.getElementById(element)
-    boton.onclick     = () => {console.log("Click")};
+let boton         = document.getElementById(element)
+boton.onclick     = () => {console.log("Click")};
 }
 
-eventAction("btnPrincipal")
-eventAction("btnAyuda")
-eventAction("btnReservas")
-eventAction("btnContacto")
+eventAction(principal);
+eventAction(ayuda);
+eventAction(reservas);
+eventAction(contacto);
+
+
 
 
 
