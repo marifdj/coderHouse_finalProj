ed44d5825c7eff26296b210cab6cf9ea0c3b2c95
Maria Florencia De Juan <florenciadejuan@gmail.com>
2021-08-12 11:36:13 -0300

    desafio de animaciones

--- proyectoFinal/elements.js
+++ proyectoFinal/elements.js
@@ -6,8 +6,20 @@ $('body').prepend( '<a class="btn btn-2">Reservas</a>');
 $('body').prepend( '<a class="btn btn-3">Ayuda</a>');
 $('body').prepend( '<a class="btn btn-4">Contacto</a>' );
 
-$('article').prepend('<h4 id="ptitle2">Titulo del segundo articulo</h4>');
+$('article').prepend('<h4 id="ptitle2">Aquí estan los productos del carrito --></h4>');
 
+
+$("p").fadeOut("slow", function(){
+    $("p").fadeIn(4000);
+}); 
+
+$(".btn btn-1").click(() => { 
+    $(".btn btn-1").toggle("fast");
+});
+
+$("#ptitle2").css("color", "#black")
+        .slideUp(4000)
+        .slideDown(3000);
 
 
 
